f94f7d1aa53976516d19e0479c7d6154aa7adf46
Andreas Nordahl <andnorda@gmail.com>
2024-02-17 10:35:18 +0100

    azure token validation

--- src/index.test.ts
+++ src/index.test.ts
@@ -16,70 +16,149 @@ const privateKey = async () => (await cachedKeyPair).privateKey;
 export const jwk = async () => exportJWK((await cachedKeyPair).publicKey);
 export const jwkPrivate = async () => exportJWK(await privateKey());
 
-export const token = async (
-  pid: string,
-  options: {
-    issuer?: string;
-    expirationTime?: string;
-    audience?: string | string[];
-  } = {},
-) =>
+export const token = async ({
+  audience,
+  issuer,
+}: {
+  audience: string | string[];
+  issuer: string;
+}) =>
   new SignJWT({
-    pid,
+    pid: "pid",
   })
     .setProtectedHeader({ alg })
-    .setAudience(options.audience ?? "idporten_audience")
-    .setIssuer(options.issuer ?? "idporten_issuer")
+    .setAudience(audience)
+    .setIssuer(issuer)
     .sign(await privateKey());
 
 describe("validate token", () => {
-  let server: SetupServer;
-
-  beforeAll(() => {
-    process.env.IDPORTEN_JWKS_URI = "http://idporten-provider.test/jwks";
-    process.env.IDPORTEN_ISSUER = "idporten_issuer";
-    process.env.IDPORTEN_AUDIENCE = "idporten_audience";
-
-    server = setupServer(
-      http.get(process.env.IDPORTEN_JWKS_URI!, async () =>
-        HttpResponse.json({ keys: [await jwk()] }),
-      ),
-    );
-    server.listen();
-  });
-
-  afterAll(() => server.close());
-
-  it("succeeds for valid token", async () => {
-    expect((await validateToken(await token("a valid token"))).isOk()).toBe(
-      true,
-    );
-  });
-
   it("fails for empty token", async () => {
     const result = await validateToken("");
     expect(result.isError() && result.getError().message).toBe("empty token");
   });
 
-  it("fails verification when issuer is not idporten", async () => {
+  it("fails with no identity provider", async () => {
     const result = await validateToken(
-      await token("a valid token", {
-        issuer: "not idporten",
+      await token({
+        audience: "idporten_audience",
+        issuer: "idporten_issuer",
       }),
     );
     expect(result.isError() && result.getError().message).toBe(
-      'unexpected "iss" claim value',
+      "no identity provider",
     );
   });
 
-  it("fails verification when audience is not idporten", async () => {
-    const result = await validateToken(
-      await token("a valid token", {
-        audience: "not idporten",
-      }),
-    );
-    expect(result.isError() && result.getError().message).toBe(
-      'unexpected "aud" claim value',
-    );
+  describe("idporten", () => {
+    let server: SetupServer;
+
+    beforeAll(() => {
+      process.env.IDPORTEN_JWKS_URI = "http://idporten-provider.test/jwks";
+      process.env.IDPORTEN_ISSUER = "idporten_issuer";
+      process.env.IDPORTEN_AUDIENCE = "idporten_audience";
+
+      server = setupServer(
+        http.get(process.env.IDPORTEN_JWKS_URI, async () =>
+          HttpResponse.json({ keys: [await jwk()] }),
+        ),
+      );
+      server.listen();
+    });
+
+    afterAll(() => server.close());
+
+    it("succeeds for valid token", async () => {
+      expect(
+        (
+          await validateToken(
+            await token({
+              audience: "idporten_audience",
+              issuer: "idporten_issuer",
+            }),
+          )
+        ).isOk(),
+      ).toBe(true);
+    });
+
+    it("fails verification when issuer is not idporten", async () => {
+      const result = await validateToken(
+        await token({
+          audience: "idporten_audience",
+          issuer: "not idporten",
+        }),
+      );
+      expect(result.isError() && result.getError().message).toBe(
+        'unexpected "iss" claim value',
+      );
+    });
+
+    it("fails verification when audience is not idporten", async () => {
+      const result = await validateToken(
+        await token({
+          audience: "not idporten",
+          issuer: "idporten_issuer",
+        }),
+      );
+      expect(result.isError() && result.getError().message).toBe(
+        'unexpected "aud" claim value',
+      );
+    });
+  });
+
+  describe("azure", () => {
+    let server: SetupServer;
+
+    beforeAll(() => {
+      process.env.AZURE_OPENID_CONFIG_JWKS_URI =
+        "http://azure-provider.test/jwks";
+      process.env.AZURE_OPENID_CONFIG_ISSUER = "azure_issuer";
+      process.env.AZURE_APP_CLIENT_ID = "azure_audience";
+
+      server = setupServer(
+        http.get(process.env.AZURE_OPENID_CONFIG_JWKS_URI, async () =>
+          HttpResponse.json({ keys: [await jwk()] }),
+        ),
+      );
+      server.listen();
+    });
+
+    afterAll(() => server.close());
+
+    it("succeeds for valid token", async () => {
+      expect(
+        (
+          await validateToken(
+            await token({
+              audience: "azure_audience",
+              issuer: "azure_issuer",
+            }),
+          )
+        ).isOk(),
+      ).toBe(true);
+    });
+
+    it("fails verification when issuer is not azure", async () => {
+      const result = await validateToken(
+        await token({
+          audience: "azure_audience",
+          issuer: "not azure",
+        }),
+      );
+      expect(result.isError() && result.getError().message).toBe(
+        'unexpected "iss" claim value',
+      );
+    });
+
+    it("fails verification when audience is not azure", async () => {
+      const result = await validateToken(
+        await token({
+          audience: "not azure",
+          issuer: "azure_issuer",
+        }),
+      );
+      expect(result.isError() && result.getError().message).toBe(
+        'unexpected "aud" claim value',
+      );
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { JWTVerifyResult, createRemoteJWKSet, jwtVerify } from "jose";
+import { createRemoteJWKSet, jwtVerify } from "jose";
 
 interface Res {
   isOk(this: Res): this is Ok;
@@ -30,18 +30,38 @@ const Result = {
 
 export const validateToken = async (token: string): Promise<Res> => {
   if (token) {
-    try {
-      await jwtVerify(
-        token,
-        createRemoteJWKSet(new URL(process.env.IDPORTEN_JWKS_URI!)),
-        {
-          issuer: process.env.IDPORTEN_ISSUER,
-          audience: process.env.IDPORTEN_AUDIENCE,
-        },
-      );
-      return Result.ok();
-    } catch (e) {
-      return Result.error(e);
+    if (process.env.AZURE_OPENID_CONFIG_ISSUER) {
+      try {
+        await jwtVerify(
+          token,
+          createRemoteJWKSet(
+            new URL(process.env.AZURE_OPENID_CONFIG_JWKS_URI!),
+          ),
+          {
+            issuer: process.env.AZURE_OPENID_CONFIG_ISSUER,
+            audience: process.env.AZURE_APP_CLIENT_ID,
+          },
+        );
+        return Result.ok();
+      } catch (e) {
+        return Result.error(e);
+      }
+    } else if (process.env.IDPORTEN_ISSUER) {
+      try {
+        await jwtVerify(
+          token,
+          createRemoteJWKSet(new URL(process.env.IDPORTEN_JWKS_URI!)),
+          {
+            issuer: process.env.IDPORTEN_ISSUER,
+            audience: process.env.IDPORTEN_AUDIENCE,
+          },
+        );
+        return Result.ok();
+      } catch (e) {
+        return Result.error(e);
+      }
+    } else {
+      return Result.error(new Error("no identity provider"));
     }
   } else {
     return Result.error(new Error("empty token"));
