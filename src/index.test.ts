b8f92227fd1ecdbda645429c56e48de0cee248f9
Andreas Nordahl <andnorda@gmail.com>
2024-02-17 09:58:03 +0100

    simple mocked jwt verify test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,59 @@
+import {
+  GenerateKeyPairResult,
+  SignJWT,
+  exportJWK,
+  generateKeyPair,
+} from "jose";
+import { HttpResponse, http } from "msw";
+import { SetupServer, setupServer } from "msw/node";
 import { validateToken } from ".";
 
-const token = (pid: string) => {
-  return pid;
-};
+const alg = "RS256";
+
+const cachedKeyPair: Promise<GenerateKeyPairResult> = generateKeyPair(alg);
+const privateKey = async () => (await cachedKeyPair).privateKey;
+
+export const jwk = async () => exportJWK((await cachedKeyPair).publicKey);
+export const jwkPrivate = async () => exportJWK(await privateKey());
+
+export const token = async (
+  pid: string,
+  options: {
+    issuer?: string;
+    expirationTime?: string;
+    audience?: string | string[];
+  } = {},
+) =>
+  new SignJWT({
+    pid,
+  })
+    .setProtectedHeader({ alg })
+    .setAudience(options.audience ?? "idporten_audience")
+    .setIssuer(options.issuer ?? "idporten_issuer")
+    .sign(await privateKey());
 
 describe("validate token", () => {
+  let server: SetupServer;
+
+  beforeAll(() => {
+    process.env.IDPORTEN_JWKS_URI = "http://idporten-provider.test/jwks";
+    process.env.IDPORTEN_ISSUER = "idporten_issuer";
+    process.env.IDPORTEN_AUDIENCE = "idporten_audience";
+
+    server = setupServer(
+      http.get(process.env.IDPORTEN_JWKS_URI!, async () =>
+        HttpResponse.json({ keys: [await jwk()] }),
+      ),
+    );
+    server.listen();
+  });
+
+  afterAll(() => server.close());
+
   it("succeeds for valid token", async () => {
-    expect((await validateToken(token("d0f96fd30a"))).isOk()).toBe(true);
+    expect((await validateToken(await token("a valid token"))).isOk()).toBe(
+      true,
+    );
   });
 
   it("fails for empty token", async () => {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import { JWTVerifyResult, createRemoteJWKSet, jwtVerify } from "jose";
+
+type Result = {
+  isOk: () => boolean;
+};
+
 const Result = {
   ok: () => ({
     isOk: () => true,
@@ -7,9 +13,22 @@ const Result = {
   }),
 };
 
-export const validateToken = async (token: string) => {
+export const validateToken = async (token: string): Promise<Result> => {
   if (token) {
-    return Result.ok();
+    try {
+      await jwtVerify(
+        token,
+        createRemoteJWKSet(new URL(process.env.IDPORTEN_JWKS_URI!)),
+        {
+          issuer: process.env.IDPORTEN_ISSUER,
+          audience: process.env.IDPORTEN_AUDIENCE,
+        },
+      );
+      return Result.ok();
+    } catch (e) {
+      console.log(e);
+      return Result.error(e.message);
+    }
   } else {
     return Result.error("Empty token");
   }
