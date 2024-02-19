08f4ec00cb3e4b8d9d49fdf3fc6516c957bc717c
Andreas Nordahl <andnorda@gmail.com>
2024-02-19 13:06:20 +0100

    refactor caching

--- src/obo.ts
+++ src/obo.ts
@@ -1,14 +1,13 @@
-import { GrantBody, Issuer } from "openid-client";
 import { createHash } from "node:crypto";
+import { GrantBody, Issuer } from "openid-client";
 import { Result } from "./result";
-import { JWTPayload, decodeJwt } from "jose";
-import SieveCache from "./cache";
+import { withCache } from "./token-cache";
 
 function sha256(content: string): string {
   return createHash("sha256").update(content).digest("hex");
 }
 
-const grantOboToken = async ({
+const grantOboToken: ({
   issuer,
   token_endpoint,
   client_id,
@@ -20,7 +19,13 @@ const grantOboToken = async ({
   client_id: string;
   jwk: string;
   grant_body: GrantBody;
-}): Promise<Result<string, Error>> => {
+}) => Promise<Result<string, Error>> = async ({
+  issuer,
+  token_endpoint,
+  client_id,
+  jwk,
+  grant_body,
+}) => {
   try {
     const { access_token } = await new new Issuer({
       issuer,
@@ -32,6 +37,7 @@ const grantOboToken = async ({
     ).grant(grant_body, {
       clientAssertionPayload: { nbf: Math.floor(Date.now() / 1000) },
     });
+
     return access_token
       ? Result.Ok(access_token)
       : Result.Error(Error("TokenSet does not contain an access_token"));
@@ -41,94 +47,43 @@ const grantOboToken = async ({
 };
 
 export const requestAzureOboToken = async (assertion: string, scope: string) =>
-  grantOboToken({
-    issuer: process.env.AZURE_OPENID_CONFIG_ISSUER!,
-    token_endpoint: process.env.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT!,
-    client_id: process.env.AZURE_APP_CLIENT_ID!,
-    jwk: process.env.AZURE_APP_JWK!,
-    grant_body: {
-      grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-      assertion,
-      scope,
-      requested_token_use: "on_behalf_of",
-    },
-  });
-
-let cache: SieveCache;
-
-const averageJwtSize = 1024; // bytes
-const maxCacheSize = 128 /* MB */ * 1024 /* KB */ * 1024; /* bytes */
-const maxCacheCapacity = Math.floor(maxCacheSize / averageJwtSize);
-
-function getCache() {
-  if (cache == undefined) {
-    cache = new SieveCache(maxCacheCapacity);
-  }
-  return cache;
-}
-
-function getNow(timestamp: number) {
-  const now = Date.now();
-  if (Math.abs(now - timestamp) < Math.abs(now - timestamp * 1000)) {
-    return now;
-  } else {
-    return Math.round(now / 1000);
-  }
-}
-
-export function secondsUntil(timestamp: number): number {
-  if (timestamp <= 0) return 0;
-  const now = getNow(timestamp);
-  if (timestamp <= now) return 0;
-  return Math.round(timestamp - now);
-}
-
-const NO_CACHE_TTL = 0;
-
-function getSecondsToExpire(payload: JWTPayload) {
-  return Math.max(
-    payload.exp ? secondsUntil(payload.exp) : NO_CACHE_TTL,
-    NO_CACHE_TTL,
+  withCache(
+    () =>
+      grantOboToken({
+        issuer: process.env.AZURE_OPENID_CONFIG_ISSUER!,
+        token_endpoint: process.env.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT!,
+        client_id: process.env.AZURE_APP_CLIENT_ID!,
+        jwk: process.env.AZURE_APP_JWK!,
+        grant_body: {
+          grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+          requested_token_use: "on_behalf_of",
+          assertion,
+          scope,
+        },
+      }),
+    sha256(assertion + scope),
   );
-}
 
 export const requestTokenxOboToken = async (
   subject_token: string,
   audience: string,
-) => {
-  const cache = getCache();
-
-  const key = sha256(subject_token + audience);
-  const cachedToken = cache.get(key);
-  if (cachedToken) {
-    const now = Math.round(Date.now() / 1000);
-    if (decodeJwt(cachedToken).exp! > now - 5) {
-      return Result.Ok<string, Error>(cachedToken);
-    }
-  }
-
-  const result = await grantOboToken({
-    issuer: process.env.TOKEN_X_ISSUER!,
-    token_endpoint: process.env.TOKEN_X_TOKEN_ENDPOINT!,
-    client_id: process.env.TOKEN_X_CLIENT_ID!,
-    jwk: process.env.TOKEN_X_PRIVATE_JWK!,
-    grant_body: {
-      grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
-      subject_token_type: "urn:ietf:params:oauth:token-type:jwt",
-      audience,
-      subject_token,
-    },
-  });
-
-  if (result.isOk()) {
-    const token = result.get();
-    const ttl = getSecondsToExpire(decodeJwt(token));
-    if (ttl > 0) {
-      cache.set(key, token, ttl);
-    }
-  }
-  return result;
-};
+) =>
+  withCache(
+    () =>
+      grantOboToken({
+        issuer: process.env.TOKEN_X_ISSUER!,
+        token_endpoint: process.env.TOKEN_X_TOKEN_ENDPOINT!,
+        client_id: process.env.TOKEN_X_CLIENT_ID!,
+        jwk: process.env.TOKEN_X_PRIVATE_JWK!,
+        grant_body: {
+          grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
+          subject_token_type: "urn:ietf:params:oauth:token-type:jwt",
+          subject_token,
+          audience,
+        },
+      }),
+    sha256(subject_token + audience),
+  );
 
 export const requestOboToken = async (
   token: string,
---
+++ src/token-cache/index.ts
@@ -0,0 +1,49 @@
+import { JWTPayload, decodeJwt } from "jose";
+import SieveCache from "./cache";
+import { Result } from "../result";
+
+let cache: SieveCache;
+
+function getCache() {
+  if (!cache) {
+    const averageJwtSize = 1024; // bytes
+    const maxCacheSize = 128 /* MB */ * 1024 /* KB */ * 1024; /* bytes */
+    const maxCacheCapacity = Math.floor(maxCacheSize / averageJwtSize);
+
+    cache = new SieveCache(maxCacheCapacity);
+  }
+  return cache;
+}
+
+function getSecondsToExpire(payload: JWTPayload): number {
+  function secondsUntil(timestamp: number): number {
+    return Math.max(Math.round(timestamp - Math.round(Date.now() / 1000)), 0);
+  }
+
+  return payload.exp ? secondsUntil(payload.exp) : 0;
+}
+
+export const withCache = async (
+  func: () => Promise<Result<string, Error>>,
+  key: string,
+): Promise<Result<string, Error>> => {
+  const cache = getCache();
+
+  const cachedToken = cache.get(key);
+  if (cachedToken) {
+    return Promise.resolve(Result.Ok(cachedToken));
+  }
+
+  return func().then((result) =>
+    result.match<Result<string, Error>>({
+      Ok: (v) => {
+        const ttl = getSecondsToExpire(decodeJwt(v));
+        if (ttl > 0) {
+          cache.set(key, v, ttl);
+        }
+        return Result.Ok(v);
+      },
+      Error: (e) => Result.Error(e),
+    }),
+  );
+};
